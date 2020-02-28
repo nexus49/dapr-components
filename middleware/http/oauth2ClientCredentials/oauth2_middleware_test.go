b95ad4a30702fd16b447db50ccb66f408d206e12
I347365 <bastian.echterhoelter@sap.com>
2020-02-28 16:26:50 +0100

    Experimenting

---
+++ middleware/http/oauth2ClientCredentials/oauth2_middleware_test.go
@@ -0,0 +1,20 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// ------------------------------------------------------------
+
+package oauth2ClientCredentials
+
+// func TestRead(t *testing.T) {
+// 	m := bindings.Metadata{}
+// 	m.Properties = map[string]string{"url": "https://google.com", "method": "a", "username": "c", "password": "d"}
+// 	hs := HTTPSource{}
+// 	err := hs.Init(m)
+// 	assert.Nil(t, err)
+
+// 	err = hs.Read(func(rr *bindings.ReadResponse) error {
+// 		assert.True(t, len(rr.Data) > 0)
+// 		return nil
+// 	})
+// 	assert.Nil(t, err)
+// }
